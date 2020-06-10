bbe7dc2a45f9cd6f058472a0fcc73140568e0f87
github username <therealgracechoi@gmail.com>
2020-06-10 00:49:14 -0700

    created store.js and imported Provider into App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Provider} from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <header className="App-header">
         <h1>hello from App!</h1>
       </header>
-    </div>
+    </Provider>
   );
 }
 
